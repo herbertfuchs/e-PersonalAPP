093b03544447148ebe2698e736f2eca7d96908a6
herbert.fuchs <hmcfuchs@gmail.com>
2022-01-07 23:08:43 -0300

    Theme import at App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,Text, View } from 'react-native';
+import { theme } from './src/global/styles/theme';
 
 export default function App() {
   return (
