f1ba41c415f8eafc006f9780bb59ec2405f25d3f
herbert.fuchs <hmcfuchs@gmail.com>
2022-01-07 23:21:46 -0300

    Files organization and template for FirstScreen

---
+++ src/Screens/FirstScreen/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StatusBar } from 'react-native';
+
+export function FirstScreen(){
+    return(
+        <View>
+            <Text>
+                Primeira tela
+            </Text>
+        </View>
+    );
+}
