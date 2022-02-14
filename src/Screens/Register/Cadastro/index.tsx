90e075a54bc839498b36b0053622f2503b32ad05
herbert.fuchs <hmcfuchs@gmail.com>
2022-02-14 15:44:40 -0300

    Register page finished, APPWRITE WORKING 100%

--- src/Screens/Register/Cadastro/index.tsx
+++ src/Screens/Register/Cadastro/index.tsx
@@ -1,33 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
-    SafeAreaView,
+    SafeAreaView, // estava sendo utilizado antes, mas acredito que seja por causa do scroll e aquelas box que tinham antes
     Image,
-    } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+    TextInput,
+    KeyboardAvoidingView } from 'react-native';
+import { RouteStackParamList } from '../../Route/RouteParamList';
+import { Appwrite } from 'appwrite';
 
 import Logo from '../../../assets/peso.png';
+import Registro from '../../../assets/registro.png';
+import { LoginButton } from '../../../components/LoginButton';
 import { styles } from './styles';
 
-export function Register(){
+export function Register({ navigation, route }: RouteStackParamList<"Register">){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+// ============================== Definindo o SDK e rotas da API ==========================================
+        const sdk = new Appwrite();
+
+        sdk
+        .setEndpoint('https://king-kong.faladev.org/v1') // Your API Endpoint
+        .setProject('61f998e0b23aeb09ff5c') // Your project ID
+        ;
+// ========================================================================================================
+
+    const Cadastro = () => {
+        let promise = sdk.account.create('unique()', `${email}`, `${password}`, `${name}`);
+
+        promise.then(function (response) {
+            console.log(response); // Success
+            console.log('Cadastrado com sucesso! Redirecionando...');
+            navigation.navigate('Login')
+        }, function (error) {
+            console.log(error); // Failure
+            console.log('Não foi possível efetuar o cadastro, tente novamente ou aguarde um instante.')
+        }); 
+    }
+
     return(
-        <SafeAreaView>
-            <View style={styles.Header}>
+        <KeyboardAvoidingView style={styles.pai}>
+            <View style={styles.header}>
                 <Image
                 source={Logo}
                 style={{width: 30, height: 30, marginTop: 75, marginBottom: 10}}
                 />
             </View>
 
-            <ScrollView>
+            <View style={styles.container}>
 
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
+                <Image 
+                source={Registro} 
+                style={styles.registerImage}
+                />
 
-            </ScrollView>
-        </SafeAreaView>
+                <TextInput 
+                style={styles.input}
+                placeholder="Seu Nome"
+                autoCorrect={false}
+                onChangeText={setName} />
+
+                <TextInput 
+                style={styles.input}
+                placeholder="Seu E-mail"
+                autoCorrect={false}
+                onChangeText={setEmail} />
+
+                <TextInput 
+                style={styles.input}
+                placeholder="Sua Senha"
+                autoCorrect={false}
+                onChangeText={setPassword} />
+
+            <View style={styles.content}>
+                <LoginButton title={'Cadastrar-se'} onPress={() => {Cadastro()}} />
+            </View>
+
+            </View>
+        </KeyboardAvoidingView>
     );
 }
--- src/Screens/Register/Cadastro/styles.ts
+++ src/Screens/Register/Cadastro/styles.ts
@@ -3,17 +3,41 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../../global/styles/theme';
 
 export const styles = StyleSheet.create({
-
-    Header: {
+    pai: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    header: {
         backgroundColor: theme.colors.temaColor,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        marginBottom: 30
     },
-
-    box: {
-        height: 300,
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        fontFamily: theme.fonts.title700,
+        marginTop: 3
+    },
+    registerImage: {
+        width: '90%',
+        height: 258
+    },
+    input: {
+        marginTop: 20,
+        padding: 5,
+        color: '#000',
         backgroundColor: theme.colors.textColor,
-        margin: 7,
-        borderRadius: 5,
+        borderRadius: 10,
+        width: '90%',
+    },
+    content: {
+        width: '75%',
+        marginTop: 25,
+        paddingHorizontal: 20
     }
 });
